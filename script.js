992f8db51fe125600349d917502bfe126a8c2f3c
ThanhZ <89237062+thqnhz@users.noreply.github.com>
2025-10-25 22:32:46 +0700

    feat: fullscreen request, desktop wallpaper

---
+++ script.js
@@ -0,0 +1,49 @@
+const fullscreenOverlay = document.getElementById("fullscreen-overlay");
+
+function showPrompt() {
+    fullscreenOverlay.style.opacity = "1";
+    fullscreenOverlay.style.pointerEvents = "auto";
+}
+
+function hidePrompt() {
+    fullscreenOverlay.style.opacity = "0";
+    fullscreenOverlay.style.pointerEvents = "none";
+}
+
+// Check for fullscreen
+document.addEventListener("fullscreenchange", () => {
+    if (document.fullscreenElement) hidePrompt();
+    else showPrompt();
+})
+
+// Check when loading page
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        if (!document.fullscreenElement) showPrompt();
+    }, 300);
+})
+
+const fullscreenBtn = document.getElementById("fullscreen-btn");
+
+fullscreenBtn.addEventListener("click", async () => {
+    try {
+        await document.documentElement.requestFullscreen();
+        hidePrompt();
+    } catch (error) {
+        alert("Failed to fullscreen. Please press F11 manually.")
+    }
+})
+
+const closeBtn = document.querySelector(".windows-button.red");
+const vscode = document.getElementById("vscode");
+const desktop = document.getElementById("desktop");
+
+closeBtn.addEventListener("click", () => {
+    vscode.classList.add("fade-out");
+    setTimeout(() => {
+        vscode.classList.add("hidden");
+
+        desktop.classList.remove("hidden");
+        requestAnimationFrame(() => desktop.classList.add("fade-in"));
+    }, 300);
+})
